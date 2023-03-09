c35242747563e2a9feaba14c20c555fe6dde9192
Bruce W. Herr II <bherr@indiana.edu>
2023-03-09 16:59:04 -0500

    [WIP] Normalizing ASCT+B table DOs

--- src/normalization/normalize-asct-b.js
+++ src/normalization/normalize-asct-b.js
@@ -1,3 +1,33 @@
-export function normalizeAsctb(environment) {
-  console.log('normalize asct-b', environment);
+import fetch from 'node-fetch';
+import { resolve } from 'path';
+import { readMetadata, validateNormalized, writeNormalized } from './utils.js';
+
+const ASCTB_API = 'https://mmpyikxkcp.us-east-2.awsapprunner.com/';
+
+export async function normalizeAsctb(environment) {
+  const obj = environment.selectedDigitalObject;
+  const metadata = readMetadata(obj);
+  const dataUrl = metadata.datatable;
+  const requestUrl =
+    ASCTB_API +
+    'v2/csv?' +
+    new URLSearchParams({
+      csvUrl: dataUrl,
+      cached: true,
+    });
+  const data = await fetch(requestUrl).then((r) => r.json());
+
+  writeNormalized(obj, metadata, data.data);
+  console.log('normalized asct-b table written to', obj.doString + '/normalized/normalized.yaml');
+
+  if (!environment.skipValidation) {
+    const isValid = validateNormalized(obj, environment.processorHome);
+    if (!isValid) {
+      console.log(
+        'normalized asct-b table was invalid! Check the errors at',
+        resolve(obj.path, 'normalized/errors.yaml')
+      );
+      return;
+    }
+  }
 }
