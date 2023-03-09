32491c3fb80ecff49ccd6cb36f6e6c3bb6109d6a
Bruce W. Herr II <bherr@indiana.edu>
2023-03-09 15:38:19 -0500

    Tweak list output for better unix postprocessing

--- src/list.js
+++ src/list.js
@@ -3,5 +3,7 @@ import { globSync } from 'glob';
 export function list(environment) {
   const glob = '**/raw';
   const digitalObjects = globSync(glob, { cwd: environment.doHome }).map((p) => p.split('/').slice(0, -1).join('/'));
-  console.log(digitalObjects.join('\n'));
+  if (digitalObjects.length > 0) {
+    console.log(digitalObjects.join('\n'));
+  }
 }
