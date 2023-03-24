6be6d11d8b5cd291f7a7b6721c42736c78806b02
Josef Hardi <josef.hardi@gmail.com>
2023-03-23 20:26:33 -0700

    Update ASCT+B data processing

--- src/normalization/normalize-asct-b.js
+++ src/normalization/normalize-asct-b.js
@@ -52,78 +52,180 @@ export async function normalizeAsctb(context) {
 }
 
 function normalizeAsctbApiResponse(data) {
-  const normalizedData = data.reduce((newData, oldRecord) => {
-    newData.push({
-      anatomical_structures: normalizeAnatomicalStructures(oldRecord.anatomical_structures),
-      cell_types: normalizeCellTypes(oldRecord.cell_types),
-      biomarkers: normalizeBiomarkers(oldRecord.biomarkers),
-      biomarkers_protein: normalizeBiomarkers(oldRecord.biomarkers_protein),
-      biomarkers_gene: normalizeBiomarkers(oldRecord.biomarkers_gene),
-      biomarkers_lipids: normalizeBiomarkers(oldRecord.biomarkers_lipids),
-      biomarkers_meta: normalizeBiomarkers(oldRecord.biomarkers_meta),
-      biomarkers_prot: normalizeBiomarkers(oldRecord.biomarkers_prot),
-      ftu_types: normalizeFtuTypes(oldRecord.ftu_types),
-      references: normalizeReferences(oldRecord.references),
-    });
-    return newData;
-  }, []);
-  return normalizedData;
+  return {
+    anatomical_structures: data.reduce((collector, row) => {
+      row.anatomical_structures.reduce(normalizeASData, collector);
+      return collector;
+    }, [{
+      id: 'UBERON:0013702',
+      type: 'AnatomicalStructure',
+      ccf_pref_label: 'body proper',
+      ccf_asctb_type: 'AS',
+      ccf_is_provisional: false,
+    }]),
+    cell_types: data.reduce((collector, row) => {
+      row.cell_types.reduce(normalizeCTData, collector);
+
+      // Add ccf_located_in relationship that is between AS and CT
+      const last_as = row.anatomical_structures.pop();
+      const last_ct = row.cell_types.pop();
+      addLocatedIn(collector, last_as, last_ct);
+
+      // Add has_biomarker relationship between CT and BM
+      const biomarkers = row.biomarkers.filter(({id, name}) => checkIfValid({id, name}))
+      addCharacterizingBiomarkers(collector, last_ct, biomarkers);
+
+      return collector;
+    }, [{
+      id: 'CL:0000000',
+      type: 'CellType',
+      ccf_pref_label: 'cell type',
+      ccf_asctb_type: 'CT',
+      ccf_is_provisional: false,
+    }]),
+    biomarkers: data.reduce((collector, row) => {
+      row.biomarkers.reduce(normalizeBMData, collector);
+      return collector;
+    }, []),
+  };
 }
 
-function normalizeAnatomicalStructures(arr, excludeIdlessObject = true) {
-  return arr
-    .filter((item) => excludeIdlessObject && item.id !== '')
-    .map((item) => ({
-      id: expandId(item.id),
-      label: item.rdfs_label,
-      preferred_name: item.name,
-    }))
-    .filter(removeDuplicate);
+function normalizeASData(accumulator, {id, name}, index, array) {
+  if (checkIfValid({id, name})) {
+    const foundEntity = accumulator.find(
+      (entity) => entity.id === generateIdWhenEmpty(id, name)
+    );
+    if (foundEntity) {
+      const oldPartOf = foundEntity.ccf_part_of;
+      const newPartOf = [
+        ...oldPartOf,
+        getPartOfEntity(array, index),
+      ];
+      foundEntity.ccf_part_of = removeDuplicates(newPartOf);
+    } 
+    else {
+      accumulator.push({
+        id: generateIdWhenEmpty(id, name),
+        type: "AnatomicalStructure",
+        ccf_pref_label: name,
+        ccf_asctb_type: "AS",
+        ccf_is_provisional: !checkNotEmpty(id),
+        ccf_part_of: [ getPartOfEntity(array, index) ], 
+      });
+    }
+  }
+  return accumulator;
 }
 
-function normalizeCellTypes(arr, excludeIdlessObject = true) {
-  return arr
-    .filter((item) => excludeIdlessObject && item.id !== '')
-    .map((item) => ({
-      id: expandId(item.id),
-      label: item.rdfs_label,
-      preferred_name: item.name,
-    }))
-    .filter(removeDuplicate);
+function getPartOfEntity(array, index) {
+  return (index === 0) 
+    ? "UBERON:0013702" // body proper
+    : generateIdWhenEmpty(array[index-1].id, array[index-1].name)
 }
 
-function normalizeBiomarkers(arr, excludeIdlessObject = true) {
-  return arr
-    .filter((item) => excludeIdlessObject && item.id !== '')
-    .map((item) => ({
-      id: expandId(item.id),
-      label: item.rdfs_label,
-      preferred_name: item.name,
-      biomarker_type: item.b_type,
-    }))
-    .filter(removeDuplicate);
+function normalizeCTData(accumulator, {id, name}, index, array) {
+  if (checkIfValid({id, name})) {
+    const foundEntity = accumulator.find(
+      (entity) => entity.id === generateIdWhenEmpty(id, name)
+    );
+    if (foundEntity) {
+      const oldCtIsa = foundEntity.ccf_ct_isa;
+      const newCtIsa = [
+        ...oldCtIsa,
+        getCtIsaEntity(array, index),
+      ];
+      foundEntity.ccf_ct_isa = removeDuplicates(newCtIsa);
+    } 
+    else {
+      accumulator.push({
+        id: generateIdWhenEmpty(id, name),
+        type: "CellType",
+        ccf_pref_label: name,
+        ccf_asctb_type: "CT",
+        ccf_is_provisional: !checkNotEmpty(id),
+        ccf_ct_isa: [ getCtIsaEntity(array, index) ], 
+        ccf_located_in: [],
+        ccf_has_biomarker_set: [],
+      });
+    }
+  }
+  return accumulator;
 }
 
-function normalizeFtuTypes(arr, excludeIdlessObject = true) {
-  return arr
-    .filter((item) => excludeIdlessObject && item.id !== '')
-    .map((item) => ({
-      id: expandId(item.id),
-      label: item.rdfs_label,
-      preferred_name: item.name,
-    }))
-    .filter(removeDuplicate);
+function getCtIsaEntity(array, index) {
+  return (index === 0) 
+    ? "CL:0000000" // cell type
+    : generateIdWhenEmpty(array[index-1].id, array[index-1].name)
 }
 
-function normalizeReferences(arr, excludeIdlessObject = true) {
-  return arr
-    .filter((item) => excludeIdlessObject && item.doi !== '')
-    .map((item) => ({
-      id: expandId(item.doi),
-      doi: printDoi(item.doi),
-      pubmed_id: item.id,
-    }))
-    .filter(removeDuplicate);
+function normalizeBMData(accumulator, {id, name, b_type}, index, array) {
+  if (checkNotEmpty(id) || checkNotEmpty(name)) {
+    const foundEntity = accumulator.find(
+      (entity) => entity.id === generateIdWhenEmpty(id, name)
+    );
+    if (!foundEntity) {
+      accumulator.push({
+        id: generateIdWhenEmpty(id, name),
+        type: "Biomarker",
+        ccf_pref_label: name,
+        ccf_asctb_type: "BM",
+        ccf_biomarker_type: b_type,
+        ccf_is_provisional: !checkNotEmpty(id),
+      });
+    }
+  }
+  return accumulator;
+}
+
+function addLocatedIn(array, {id: as_id, name: as_name}, {id: ct_id, name: ct_name}) {
+  const foundEntity = array.find(
+    (entity) => entity.id === generateIdWhenEmpty(ct_id, ct_name)
+  );
+  if (foundEntity) {
+    const oldLocatedIn = foundEntity.ccf_located_in;
+    const newLocatedIn = [
+      ...oldLocatedIn,
+      generateIdWhenEmpty(as_id, as_name)
+    ];
+    foundEntity.ccf_located_in = removeDuplicates(newLocatedIn);
+  }
+}
+
+function addCharacterizingBiomarkers(array, {id: ct_id, name: ct_name}, biomarkers) {
+  const foundEntity = array.find(
+    (entity) => entity.id === generateIdWhenEmpty(ct_id, ct_name)
+  );
+  if (foundEntity) {
+    const oldHasBiomarker = foundEntity.ccf_has_biomarker_set;
+    const newHasBiomarker = [
+      ...oldHasBiomarker,
+      { 
+        members: removeDuplicates(biomarkers.map(
+          ({id: bm_id, name: bm_name}) => generateIdWhenEmpty(bm_id, bm_name)
+        ))
+      }
+    ];
+    foundEntity.ccf_has_biomarker_set = removeDuplicates(newHasBiomarker);
+  }
+}
+
+function generateIdWhenEmpty(id, name) {
+  if (checkNotEmpty(id) && id.match(/\w+:\w+/)) {
+    return id;
+  }
+  const suffix = name.trim()
+    .toLowerCase()
+    .replace(/\W+/g, '-')
+    .replace(/[^a-z0-9-]+/g, '');
+  return `ASCTB-TEMP:${suffix}`;
+}
+
+function checkIfValid({id, name}) {
+  return checkNotEmpty(id) || checkNotEmpty(name);
+}
+
+function checkNotEmpty(str) {
+  return str && str.trim() !== "";
 }
 
 function expandId(id) {
@@ -140,6 +242,6 @@ function printDoi(doi) {
   return doi.replace(/^DOI:/, '').replace(/\s+/g, '');
 }
 
-function removeDuplicate(obj, index, arr) {
-  return index === arr.findIndex((item) => item.id === obj.id);
+function removeDuplicates(array) {
+  return Array.from(new Set(array.map(JSON.stringify)), JSON.parse);
 }
