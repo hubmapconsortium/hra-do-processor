0177fa6ee2a63445b9c08484ca5817229c552dae
Josef Hardi <josef.hardi@gmail.com>
2023-04-12 15:35:16 -0700

    Create procedures to override the schema id and clean up the changes

--- src/enrichment/enrich-asct-b.js
+++ src/enrichment/enrich-asct-b.js
@@ -1,6 +1,24 @@
+import { resolve } from 'path';
+import sh from 'shelljs';
 import { convertNormalizedToOwl } from './utils.js';
 
 export function enrichAsctb(context) {
   // TODO: do more than a straight conversion to rdf
+  overrideSchemaId(context);
   convertNormalizedToOwl(context);
+  revertChanges(context);
+}
+
+function overrideSchemaId(context) {
+  const { selectedDigitalObject: obj, processorHome } = context;
+  const newId = `http://purl.humanatlas.io/${obj.doString}`;
+  const schema = resolve(processorHome, 'schemas/generated/linkml', `${obj.type}.yaml`);
+  sh.exec(`sed -i.bak 's|^id:.*|id: ${newId}|' ${schema}`);
+}
+
+function revertChanges(context) {
+  const { selectedDigitalObject: obj, processorHome } = context;
+  const originalSchema = resolve(processorHome, 'schemas/generated/linkml', `${obj.type}.yaml.bak`);
+  const schema = resolve(processorHome, 'schemas/generated/linkml', `${obj.type}.yaml`);
+  sh.exec(`mv ${originalSchema} ${schema}`);
 }
