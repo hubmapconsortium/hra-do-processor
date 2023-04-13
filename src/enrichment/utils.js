5826191df9bebfde2cb0808171882aaa5963b1ae
Josef Hardi <josef.hardi@gmail.com>
2023-04-13 11:37:47 -0700

    Rename variables

--- src/enrichment/utils.js
+++ src/enrichment/utils.js
@@ -6,34 +6,32 @@ export function convertNormalized(context) {
   const { selectedDigitalObject: obj, processorHome, skipValidation } = context;
 
   const schema = resolve(processorHome, 'schemas/generated/linkml', `${obj.type}.yaml`);
-  const normalizedPath = resolve(obj.path, 'normalized/normalized.yaml');
-  const enrichedPath = resolve(obj.path, 'enriched/enriched.ttl');
+  const input = resolve(obj.path, 'normalized/normalized.yaml');
+  const output = resolve(obj.path, 'enriched/enriched.ttl');
 
   const results = sh.exec(
-    `linkml-convert ${skipValidation ? '--no-validate' : ''} --schema ${schema} ${normalizedPath} -o ${enrichedPath}`
+    `linkml-convert ${skipValidation ? '--no-validate' : ''} --schema ${schema} ${input} -o ${output}`
   );
   const success = results.code !== 1;
   if (!success) {
     console.log(chalk.red('Enrichment failed. See errors above.'));
+    exit();
   }
-
-  return success;
 }
 
 export function convertNormalizedToOwl(context) {
-  const { selectedDigitalObject: obj, processorHome, skipValidation } = context;
+  const { selectedDigitalObject: obj, processorHome } = context;
 
   const schema = resolve(processorHome, 'schemas/generated/linkml', `${obj.type}.yaml`);
-  const normalizedPath = resolve(obj.path, 'normalized/normalized.yaml');
-  const enrichedPath = resolve(obj.path, 'enriched/enriched.ttl');
+  const input = resolve(obj.path, 'normalized/normalized.yaml');
+  const output = resolve(obj.path, 'enriched/enriched.ttl');
 
   const results = sh.exec(
-    `linkml-data2owl --output-type ttl --schema ${schema} ${normalizedPath} -o ${enrichedPath}`
+    `linkml-data2owl --output-type ttl --schema ${schema} ${input} -o ${output}`
   );
   const success = results.code !== 1;
   if (!success) {
     console.log(chalk.red('Enrichment failed. See errors above.'));
+    exit();
   }
-
-  return success;
 }
