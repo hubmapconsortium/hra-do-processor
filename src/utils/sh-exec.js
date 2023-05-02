e01d7b023b6ccb44fe1fb56736ce68981e121b4b
Josef Hardi <josef.hardi@gmail.com>
2023-05-02 12:38:16 -0700

    Write a helper function to log errors

--- src/utils/sh-exec.js
+++ src/utils/sh-exec.js
@@ -1,4 +1,7 @@
 import sh from 'shelljs';
+import { writeFileSync } from 'fs';
+import { dump } from 'js-yaml';
+import chalk from 'chalk';
 
 export function throwOnError(command, message) {
   const response = sh.exec(command);
@@ -6,4 +9,26 @@ export function throwOnError(command, message) {
   if (!success) {
     throw new Error(message);
   }
+}
+
+export function logOnError(command, message, { errorFile, errorParser }) {
+  const response = sh.exec(command, { silent: true });
+  const { stdout, stderr, code } = response;
+  const success = code !== 1;
+  if (!success) {
+    let errorMessage = message;
+    if (stderr !== '') {
+      if (errorParser) {
+        errorMessage = errorParser(stderr);
+      }
+    } else if (stdout !== '') {
+      if (errorParser) {
+        errorMessage = errorParser(stdout);
+      }
+    }
+    sh.rm('-f', errorFile); // Clear previous errors file
+    writeFileSync(errorFile, dump(errorMessage));
+    console.log(chalk.red(message));
+  }
+  return success;
 }
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -3,36 +3,26 @@ import { writeFileSync } from 'fs';
 import { dump } from 'js-yaml';
 import { resolve } from 'path';
 import sh from 'shelljs';
+import { logOnError } from './sh-exec.js';
 
 export function validateNormalized(context) {
   const { selectedDigitalObject: obj, processorHome, skipValidation } = context;
 
-  // Clear previous errors file before validating
-  const errorFile = resolve(obj.path, 'normalized/errors.yaml');
-
-  sh.rm('-f', errorFile);
   if (skipValidation) {
     return true;
   }
 
   const jsonSchema = resolve(processorHome, 'schemas/generated/json-schema', `${obj.type}.schema.json`);
   const normalizedPath = resolve(obj.path, 'normalized/normalized.yaml');
-  const resultString = sh.exec(`check-jsonschema -o json --schemafile ${jsonSchema} ${normalizedPath}`, {
-    silent: true,
-  });
-  const results = JSON.parse(resultString);
-  if (results.status === 'fail') {
-    writeFileSync(errorFile, dump(results));
-  }
+  const errorFile = resolve(obj.path, 'normalized/errors.yaml');
 
-  const isValid = results.status !== 'fail';
-  if (!isValid) {
-    console.log(
-      chalk.red('Normalized', obj.type, 'digital object was invalid!'),
-      'Check errors at',
-      resolve(obj.path, 'normalized/errors.yaml')
-    );
-  }
-
+  const isValid = logOnError(
+    `check-jsonschema -o json --schemafile ${jsonSchema} ${normalizedPath}`,
+    `ValidationError: Normalized ${obj.type} digital object was invalid!`,
+    {
+      errorFile: errorFile,
+      errorParser: (message) => (JSON.parse(message).errors),
+    }
+  );
   return isValid;
 }
