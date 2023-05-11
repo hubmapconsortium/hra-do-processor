ecf10539ac1305e5e1a51c775b538f47a5f86a24
Bruce W. Herr II <bherr@indiana.edu>
2023-05-11 16:32:49 -0400

    Initial deployment implementation

--- src/deployment/deploy.js
+++ src/deployment/deploy.js
@@ -1,4 +1,17 @@
+import { resolve } from 'path';
+import sh from 'shelljs';
+import { readMetadata } from '../normalization/utils.js';
 
 export function deploy(context) {
-  console.log('deploy', context.selectedDigitalObject.doString, 'not implemented (yet)');
+  const obj = context.selectedDigitalObject;
+  const deployPath = resolve(context.deploymentHome, obj.doString);
+  const metadata = readMetadata(obj.path);
+
+  sh.mkdir('-p', resolve(deployPath, 'assets'));
+  sh.cp(resolve(obj.path, 'packaged/*'), deployPath);
+  sh.cp(resolve(obj.path, 'enriched/enriched.ttl'), resolve(deployPath, 'dataset.ttl'));
+
+  for (const file of metadata.datatable) {
+    sh.cp(resolve(obj.path, 'raw', file), resolve(deployPath, 'assets', file));
+  }
 }
