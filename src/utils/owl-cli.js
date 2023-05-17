582c99ca20d0acef0634f03a6c028a658982feef
Bruce W. Herr II <bherr@indiana.edu>
2023-05-17 15:07:30 -0400

    Improve blank node pattern on collection DOs

--- src/utils/owl-cli.js
+++ src/utils/owl-cli.js
@@ -10,12 +10,13 @@ export function mergeTurtles(outputPath, prefixesPath, inputPaths) {
     .map(([key, value]) => `-p${key}=${value}`)
     .join(' ');
 
+  const blankNode = `_:${outputPath.split('/').slice(-5, -3).join('_')}_0`;
   throwOnError(
-    `cat ${inputPaths.join(' ')} | owl-cli write -i turtle -o ntriple - - > ${outputPath}.nt`,
+    `cat ${inputPaths.join(' ')} | owl-cli write --anonymousNodeIdPattern ${blankNode} -i turtle -o ntriple - - > ${outputPath}.nt`,
     `failed to merge ${outputPath}`
   );
   throwOnError(
-    `owl-cli write --writeRdfType ${prefixesString} ${outputPath}.nt ${outputPath} && rm -f ${outputPath}.nt`,
+    `owl-cli write --anonymousNodeIdPattern ${blankNode} --writeRdfType ${prefixesString} ${outputPath}.nt ${outputPath} && rm -f ${outputPath}.nt`,
     `failed to prettify ${outputPath}`
   );
 }
