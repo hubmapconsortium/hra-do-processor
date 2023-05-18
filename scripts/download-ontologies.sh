a7b13f0bbf6db4c302d5dfcc983351caa777cbde
Josef Hardi <josef.hardi@gmail.com>
2023-05-18 16:38:16 -0500

    Standardize the downloaded reference ontologies in OWL/XML format

--- scripts/download-ontologies.sh
+++ scripts/download-ontologies.sh
@@ -14,9 +14,8 @@ curl -L "$OBO_BASE_URL/uberon/uberon-base.owl" \
      --retry 4 \
      --max-time 200 && \
 robot convert -i "$MIRROR_DIR/uberon.owl" \
-     --format ttl \
-     -o "$MIRROR_DIR/uberon.ttl" && \
-rm -rf "$MIRROR_DIR/uberon.owl"
+     --format owl \
+     -o "$MIRROR_DIR/uberon.owl"
 
 # Download the latest FMA ontology
 curl -L "http://sig.biostr.washington.edu/share/downloads/fma/release/latest/fma.zip" \
@@ -25,9 +24,9 @@ curl -L "http://sig.biostr.washington.edu/share/downloads/fma/release/latest/fma
      --max-time 200 && \
 unzip $MIRROR_DIR/fma.zip -x license.txt pun_fma.owl README.txt -d $MIRROR_DIR && \
 robot convert -i "$MIRROR_DIR/fma.owl" \
-     --format ttl \
-     -o "$MIRROR_DIR/fma.ttl" && \
-rm -rf $MIRROR_DIR/fma.owl $MIRROR_DIR/fma.zip
+     --format owl \
+     -o "$MIRROR_DIR/fma.owl"
+rm -rf $MIRROR_DIR/fma.zip
 
 # Download the latest CL ontology
 curl -L "$OBO_BASE_URL/cl/cl-base.owl" \
@@ -35,9 +34,8 @@ curl -L "$OBO_BASE_URL/cl/cl-base.owl" \
      --retry 4 \
      --max-time 200 && \
 robot convert -i "$MIRROR_DIR/cl.owl" \
-     --format ttl \
-     -o "$MIRROR_DIR/cl.ttl" && \
-rm -rf "$MIRROR_DIR/cl.owl"
+     --format owl \
+     -o "$MIRROR_DIR/cl.owl"
 
 # Download the latest HGNC ontology
 curl -L "https://github.com/musen-lab/hgnc2owl/raw/main/hgnc.owl.gz" \
@@ -46,6 +44,14 @@ curl -L "https://github.com/musen-lab/hgnc2owl/raw/main/hgnc.owl.gz" \
      --max-time 200 && \
 gunzip -f "$MIRROR_DIR/hgnc.owl.gz" && \
 robot convert -i "$MIRROR_DIR/hgnc.owl" \
-     --format ttl \
-     -o "$MIRROR_DIR/hgnc.ttl" && \
-rm -rf "$MIRROR_DIR/hgnc.owl" 
+     --format owl \
+     -o "$MIRROR_DIR/hgnc.owl"
+
+# Download the latest RO ontology
+curl -L "$OBO_BASE_URL/ro/ro.owl" \
+     --create-dirs -o "$MIRROR_DIR/ro.owl" \
+     --retry 4 \
+     --max-time 200 && \
+robot convert -i "$MIRROR_DIR/ro.owl" \
+     --format owl \
+     -o "$MIRROR_DIR/ro.owl"
