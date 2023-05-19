80084fbe901a50b836329fedc8a50ac0d19a5d16
Josef Hardi <josef.hardi@gmail.com>
2023-05-19 12:35:58 -0500

    Refactor enrich-asct-b code

--- src/enrichment/enrich-asct-b.js
+++ src/enrichment/enrich-asct-b.js
@@ -1,53 +1,57 @@
 import { resolve } from 'path';
-import { error, header, info } from '../utils/logging.js';
-import { collectEntities, extractClassHierarchy, mergeOntologies } from '../utils/robot.js';
+import { convertNormalizedToOwl, 
+         downloadValidationResult, 
+         cleanTemporaryFiles } from './utils.js';
+import { collectEntities, extractClassHierarchy, merge } from '../utils/robot.js';
 import { throwOnError } from '../utils/sh-exec.js';
-import { convertNormalizedToOwl, downloadValidationResult, cleanTemporaryFiles } from './utils.js';
+import { error, header, info, more } from '../utils/logging.js';
 
 export function enrichAsctb(context) {
   header(context, 'run-enrich');
   try {
+    const { selectedDigitalObject: obj } = context;
+
     // Convert normalized data to graph data (.ttl)
     overrideSchemaId(context);
-    const enrichedData = convertNormalizedToOwl(context);
+    const normalizedPath = resolve(obj.path, 'normalized/normalized.yaml');
+    const graphPath = resolve(obj.path, 'enriched/graph.ttl');
+    convertNormalizedToOwl(context, normalizedPath, graphPath);
     revertChanges(context);
     
     info('Starting the data enrichment...')
 
-    // Include asssertions from the CCF validation tool to enrich the graph data
-    const validationResults = downloadValidationResult(context);
+    // Download CCF validation result to enrich the graph data
+    const validationPath = downloadValidationResult(context);
+    const enrichedPath = resolve(obj.path, 'enriched.owl')
+    merge([graphPath, validationPath], enrichedPath);
 
-    // Merge all the resources
-    const validatedData = mergeOntologies(context, [
-      enrichedData,
-      validationResults
-    ]);
-
-    // Include assertions from the reference ontologies to enrich the graph data
+    // Extract terms from reference ontologies to enrich the graph data
     const uberonEntities = collectEntities(context, "uberon", validatedData);
-    const uberonExtract = extractClassHierarchy(context, "uberon",
+    const uberonExtractPath = extractClassHierarchy(context, "uberon",
       "http://purl.obolibrary.org/obo/UBERON_0001062", uberonEntities);
     
     const fmaEntities = collectEntities(context, "fma", validatedData);
-    const fmaExtract = extractClassHierarchy(context, "fma", 
+    const fmaExtractPath = extractClassHierarchy(context, "fma", 
       "http://purl.org/sig/ont/fma/fma62955", fmaEntities);
 
     const clEntities = collectEntities(context, "cl", validatedData);
-    const clExtract = extractClassHierarchy(context, "cl",
+    const clExtractPath = extractClassHierarchy(context, "cl",
       "http://purl.obolibrary.org/obo/CL_0000000", clEntities);
 
     const hgncEntities = collectEntities(context, "hgnc", validatedData);
-    const hgncExtract = extractClassHierarchy(context, "hgnc", 
+    const hgncExtractPath = extractClassHierarchy(context, "hgnc", 
       "http://purl.bioontology.org/ontology/HGNC/gene", hgncEntities);
 
-    // Merge all the resources
-    mergeOntologies(context, [
-      validatedData,
-      uberonExtract,
-      fmaExtract,
-      clExtract,
-      hgncExtract
-    ]);
+    merge([enrichedPath,
+      uberonExtractPath,
+      fmaExtractPath,
+      clExtractPath,
+      hgncExtractPath
+    ], enrichedPath);
+
+    const turtleEnrichedPath = resolve(obj.path, 'enriched/enriched.ttl');
+    info(`Creating file: ${turtleEnrichedPath}`);
+    convert(enrichedPath, turtleEnrichedPath, "ttl");
   }
   catch (e) {
     error(e);
@@ -78,4 +82,41 @@ function revertChanges(context) {
     `mv ${originalSchema} ${schema}`,
     'Revert schema changes failed.'
   );
+}
+
+function downloadValidationResult(context, useNightlyBuild=true) {
+  const { name, path } = context.selectedDigitalObject;
+  
+  const baseUrl = "https://raw.githubusercontent.com/hubmapconsortium/ccf-validation-tools/master/owl";
+  if (!useNightlyBuild) {
+    baseUrl = `${baseUrl}/last_official_ASCTB_release`
+  }
+  const organName = findOrganName(name);
+  const input = `${baseUrl}/${organName}_extended.owl`;
+
+  more(`Downloading from ccf-validation-tools: ${input}`);
+
+  const download = resolve(path, `enriched/${organName}_extended.owl`);
+  const output = resolve(path, `enriched/${name}-validation.owl`);
+  throwOnError(
+    `wget -nc -nv -q ${input} -O ${download} && \
+     robot convert --input ${download} --format owl -o ${output}`,
+    'Download validation result failed.'
+  )
+  return output;
+}
+
+function findOrganName(name) {
+  const normalizeString = name.replace(/^(vh-)/, "");
+  const titleCaseString = normalizeString.charAt(0).toUpperCase() + normalizeString.slice(1);
+  const snakeCaseString = titleCaseString.replace(/-/g, "_");
+
+  let outputName = snakeCaseString;
+  // Handle special cases
+  if (snakeCaseString === "Bone_marrow") {
+    outputName = "Bone-Marrow";
+  } else if (snakeCaseString === "Spinal_cord") {
+    outputName = "Spinal_Cord";
+  }
+  return outputName;
 }
--- src/enrichment/utils.js
+++ src/enrichment/utils.js
@@ -22,23 +22,15 @@ export function convertNormalized(context) {
   return output;
 }
 
-export function convertNormalizedToOwl(context) {
+export function convertNormalizedToOwl(context, inputPath, outputPath) {
   const { selectedDigitalObject: obj, processorHome } = context;
-
-  const schema = resolve(processorHome, 'schemas/generated/linkml', `${obj.type}.yaml`);
-  const input = resolve(obj.path, 'normalized/normalized.yaml');
-  const output = resolve(obj.path, 'enriched/enriched.ttl');
-  const errorFile = resolve(obj.path, 'enriched/errors.yaml');
-
-  info(`Using 'linkml-data2owl' to transform ${input}`)
+  const schemaPath = resolve(processorHome, 'schemas/generated/linkml', `${obj.type}.yaml`);
+  info(`Using 'linkml-data2owl' to transform ${inputPath}`)
   throwOnError(
-    `linkml-data2owl --output-type ttl --schema ${schema} ${input} -o ${output}`,
+    `linkml-data2owl --output-type ttl --schema ${schemaPath} ${inputPath} -o ${outputPath}`,
     'Converting to OWL failed.',
     (message) => (message.replace(/(.*\n)+TypeError:(.*)/, '$2').trim())
   );
-  info(`Enriched digital object written to ${output}`);
-
-  return output;
 }
 
 export function runCompleteClosure(context, inputPath, outputPath) {
@@ -46,44 +38,6 @@ export function runCompleteClosure(context, inputPath, outputPath) {
   const closurePath = resolve(obj.path, 'enriched/closure.ttl');
   redundant(inputPath, closurePath);
   merge([inputPath, closurePath], outputPath);
-}
-
-export function downloadValidationResult(context, useNightlyBuild=true) {
-  const { name, path } = context.selectedDigitalObject;
-  
-  const baseUrl = "https://raw.githubusercontent.com/hubmapconsortium/ccf-validation-tools/master/owl";
-  if (!useNightlyBuild) {
-    baseUrl = `${baseUrl}/last_official_ASCTB_release`
-  }
-  const organName = findOrganName(name);
-  const input = `${baseUrl}/${organName}_extended.owl`;
-
-  more(`Downloading from ccf-validation-tools: ${input}`);
-
-  const download = resolve(path, `enriched/${name}-validation.owl`);
-  const output = resolve(path, `enriched/${name}-validation.ttl`);
-  throwOnError(
-    `wget -nc -nv -q ${input} -O ${download} && \
-     robot convert --input ${download} --format ttl -o ${output}`,
-    'Download validation failed.'
-  )
-
-  return output;
-}
-
-function findOrganName(name) {
-  const normalizeString = name.replace(/^(vh-)/, "");
-  const titleCaseString = normalizeString.charAt(0).toUpperCase() + normalizeString.slice(1);
-  const snakeCaseString = titleCaseString.replace(/-/g, "_");
-
-  let outputName = snakeCaseString;
-  // Handle special cases
-  if (snakeCaseString === "Bone_marrow") {
-    outputName = "Bone-Marrow";
-  } else if (snakeCaseString === "Spinal_cord") {
-    outputName = "Spinal_Cord";
-  }
-  return outputName;
 }
 
 export function cleanTemporaryFiles(context) {
