322f60bb14ee1a070cdcb8c87083afd18a0f664c
Josef Hardi <josef.hardi@gmail.com>
2023-05-25 10:44:52 -0700

    Reformat command to multiline

--- src/utils/blazegraph.js
+++ src/utils/blazegraph.js
@@ -9,12 +9,16 @@ export function mergeTurtles(outputPath, _prefixesPath, ontologyPaths) {
   const tempGraph = 'http://temp';
   const inputFiles = ontologyPaths.join(' ');
   throwOnError(
-    `blazegraph-runner load --journal=${tempJournal} --use-ontology-graph=false --graph="${tempGraph}" --informat=turtle ${inputFiles}`,
+    `blazegraph-runner load --journal=${tempJournal} \
+        --use-ontology-graph=false \
+        --graph="${tempGraph}" \
+        --informat=turtle ${inputFiles}`,
     `${inputFiles} failed to load`
   );
-
   throwOnError(
-    `blazegraph-runner dump --journal=${tempJournal} --graph="${tempGraph}" --outformat=turtle ${outputPath}`,
+    `blazegraph-runner dump --journal=${tempJournal} \
+        --graph="${tempGraph}" \
+        --outformat=turtle ${outputPath}`,
     `${outputPath} failed to dump`
   );
   sh.rm('-f', tempJournal);
@@ -25,7 +29,12 @@ export function loadDoIntoTripleStore(context, journalPath) {
   const graph = obj.iri;
   const data = resolve(obj.path, 'enriched/enriched.ttl');
   throwOnError(
-    `owl-cli write -i turtle -o ntriple ${data} ${journalPath}.temp && blazegraph-runner load --journal=${journalPath} --use-ontology-graph=false --graph="${graph}" --informat=ntriples ${journalPath}.temp && rm -f ${journalPath}.temp`,
+    `owl-cli write -i turtle -o ntriple ${data} ${journalPath}.temp && \
+     blazegraph-runner load --journal=${journalPath} \
+        --use-ontology-graph=false \
+        --graph="${graph}" \
+        --informat=ntriples ${journalPath}.temp && \
+     rm -f ${journalPath}.temp`,
     `${data} failed to load`
   );
 }
--- src/utils/reify.js
+++ src/utils/reify.js
@@ -13,12 +13,14 @@ export function reifyTurtle(context, inputPath, journal) {
   for (const [extension, type] of Object.entries(FORMATS)) {
     throwOnError(
       `rdfpipe --output-format ${type} ${inputPath} > ${basePath}.${extension}`,
-      `failed to reify ${inputPath} to ${type}`
+      `Failed to reify ${inputPath} to ${type}`
     );
   }
 
   throwOnError(
-    `blazegraph-runner dump --journal=${journal} --graph="${graph}" --outformat=nquads ${basePath}.nq`,
-    `failed to reify ${inputPath} to n-quads`
+    `blazegraph-runner dump --journal=${journal} \
+        --graph="${graph}" \
+        --outformat=nquads ${basePath}.nq`,
+    `Failed to reify ${inputPath} to n-quads`
   );
 }
