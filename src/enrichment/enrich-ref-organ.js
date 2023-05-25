141a04a3ca26f3a76c783d6be4ed393d7c8814e8
Bruce W. Herr II <bherr@indiana.edu>
2023-05-25 18:19:39 -0400

    Made ref-organs also a knowledge base

--- src/enrichment/enrich-ref-organ.js
+++ src/enrichment/enrich-ref-organ.js
@@ -1,8 +1,30 @@
-import { header } from '../utils/logging.js';
-import { convertNormalized, prettifyEnriched } from './utils.js';
+import { resolve } from 'path';
+import { header, info } from '../utils/logging.js';
+import { cleanTemporaryFiles, convertNormalized, convertNormalizedToOwl, prettifyEnriched } from './utils.js';
+import { convert, merge } from '../utils/robot.js';
 
 export function enrichRefOrgan(context) {
   header(context, 'run-enrich');
-  convertNormalized(context);
-  prettifyEnriched(context);
+  const { selectedDigitalObject: obj } = context;
+  const normalizedPath = resolve(obj.path, 'normalized/normalized.yaml');
+  const ontologyPath = resolve(obj.path, 'enriched/ontology.ttl');
+
+  try {
+    convertNormalizedToOwl(context, normalizedPath, ontologyPath);
+    convertNormalized(context);
+
+    const turtleEnrichedPath = resolve(obj.path, 'enriched/enriched.ttl');
+    const enrichedPath = resolve(obj.path, 'enriched/enriched.owl');
+    merge([ontologyPath, turtleEnrichedPath], enrichedPath);
+
+    info(`Creating ref-organ: ${turtleEnrichedPath}`);
+    convert(enrichedPath, turtleEnrichedPath, 'ttl');
+    prettifyEnriched(context);
+  } catch (e) {
+    error(e);
+  } finally {
+    // Clean up
+    info('Cleaning up temporary files.');
+    cleanTemporaryFiles(context);
+  }
 }
