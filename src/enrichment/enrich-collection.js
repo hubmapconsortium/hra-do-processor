53f57764345aaa548d724ba6222287e765d62cc6
Josef Hardi <josef.hardi@gmail.com>
2023-06-09 17:19:48 -0700

    Run complete closure in the 'collection' type

--- src/enrichment/enrich-collection.js
+++ src/enrichment/enrich-collection.js
@@ -3,7 +3,7 @@ import { load } from 'js-yaml';
 import { resolve } from 'path';
 import { error, header, info, more } from '../utils/logging.js';
 import { convert, merge } from '../utils/robot.js';
-import { cleanTemporaryFiles, convertNormalizedToOwl } from './utils.js';
+import { cleanTemporaryFiles, convertNormalizedToOwl, runCompleteClosure, logOutput } from './utils.js';
 
 export function enrichCollection(context) {
   header(context, 'run-enrich');
@@ -11,34 +11,44 @@ export function enrichCollection(context) {
     const { selectedDigitalObject: obj } = context;
 
     const normalizedPath = resolve(obj.path, 'normalized/normalized.yaml');
-    const ontologyPath = resolve(obj.path, 'enriched/ontology.ttl');
-    convertNormalizedToOwl(context, normalizedPath, ontologyPath);
+    const baseInputPath = resolve(obj.path, 'enriched/base-input.ttl');
+    convertNormalizedToOwl(context, normalizedPath, baseInputPath);
+    logOutput(baseInputPath);
 
     info(`Reading data: ${normalizedPath}`);
     const digitalObjects = load(readFileSync(normalizedPath))['data'];
 
-    info('Validating digital objects in the collection.');
+    info('Validating digital objects in the collection...');
     validateCollection(context, digitalObjects);
 
+    info('Consolidating all collection members...')
     info('Merging files:');
     const doPaths = [];
-    doPaths.push(ontologyPath);
+    doPaths.push(baseInputPath);
     digitalObjects.forEach((doId) => doPaths.push(resolve(context.doHome, doId, 'enriched/enriched.ttl')));
-    const enrichedPath = resolve(obj.path, 'enriched/enriched.owl');
+    const enrichedMergePath = resolve(obj.path, 'enriched/enriched-merge.owl');
     for (const doPath of doPaths) {
       more(` -> ${doPath}`);
     }
-    merge(doPaths, enrichedPath);
+    merge(doPaths, enrichedMergePath);
+    logOutput(enrichedMergePath);
 
-    const turtleEnrichedPath = resolve(obj.path, 'enriched/enriched.ttl');
-    info(`Creating collection: ${turtleEnrichedPath}`);
-    convert(enrichedPath, turtleEnrichedPath, 'ttl');
+    const enrichedPath = resolve(obj.path, 'enriched/enriched.ttl');
+    info(`Creating collection: ${enrichedPath}`);
+    convert(enrichedMergePath, enrichedPath, 'ttl');
+
+    info('Optimizing collection graph...')
+    const redundantPath = resolve(obj.path, 'enriched/redundant.ttl');
+    runCompleteClosure(enrichedPath, redundantPath);
+    logOutput(redundantPath);
+
   } catch (e) {
     error(e);
   } finally {
     // Clean up
     info('Cleaning up temporary files.');
     cleanTemporaryFiles(context);
+    info('Done.');
   }
 }
 
