fbda45cf5358b4fe8d092470ca92b138d3c33305
akhileshgowda <akhileshgowda7@gmail.com>
2023-07-05 14:16:45 -0400

    used Semver to normalize

--- src/finalizing/derive-latest.js
+++ src/finalizing/derive-latest.js
@@ -12,65 +12,48 @@ import semver from 'semver';
 export async function deriveLatest(context) {
   //Extracting path
   const glob = '**/graph.ttl';
-  const digitalObjects = globSync(glob, { cwd: context.deployHome }).map((p) => p.split('/').slice(0, -2).join('/'));
+  const digitalObjects = new Set(
+    globSync(glob, { cwd: context.deployHome }).map((p) => p.split('/').slice(0, -2).join('/'))
+  );
 
-  for (const digitalObject of digitalObjects) {
+  const filteredDigitalObjects = Array.from(digitalObjects).filter(
+    (object) => !object.includes('/latest') && !object.includes('/draft')
+  );
+
+  for (const digitalObject of filteredDigitalObjects) {
     //Getting list of versions
-    const versions = readdirSync(digitalObject).filter((v) => v != 'latest');
-
+    const versions = readdirSync(digitalObject).filter((v) => v.startsWith('v'));
     const obj = digitalObject.split('/').slice(0, 3).join('/');
-    try {
-      if (fs.existsSync(`${obj}/latest`)) {
-        sh.rm('-r', `${obj}/latest`);
-      }
-    } catch (error) {
-      console.error('Error while deleting latest directory', error);
-    }
+
     let sortedVersions = versions;
+    const dirMap = {}; // To map directory to valid semver versions
 
+    //normalizing versions to be processed by semver
     if (versions.length > 1) {
-      //Sorting the version in descending order
-      sortedVersions = versions.sort(async (a, b) => {
-        let versionA = a.replace('v', '');
-        let versionB = b.replace('v', '');
-
-        versionA = await validateVersion(versionA);
-        versionB = await validateVersion(versionB);
-
-        // Skip processing invalid versions
-        if (!semver.valid(versionA) || !semver.valid(versionB)) {
-          console.error('Invalid Version');
-        }
-        return semver.gt(versionA, versionB);
-      });
+      sortedVersions = versions
+        .map((version) => {
+          let coercedVersion = semver.coerce(version);
+          coercedVersion = semver.clean(coercedVersion.toString());
+          dirMap[coercedVersion] = version;
+          return coercedVersion;
+        })
+        .sort((a, b) => {
+          return semver.gt(a, b) ? -1 : 1;
+        });
+    } else {
+      console.warn('There Should be atleast one version');
     }
 
     // Getting the latest version
-    const latestVersion = sortedVersions[sortedVersions.length - 1];
+    let latestVersion = dirMap[sortedVersions[0]];
     const latestObj = `${obj}/${latestVersion}`;
 
-    // Writing the latest version to the 'latest' directory
+    if (fs.existsSync(`${obj}/latest`)) {
+      sh.rm('-r', `${obj}/latest`);
+    }
+
+    // Copying the latest version to the 'latest' directory
     sh.mkdir(`${obj}/latest`);
     sh.cp('-r', `${latestObj}`, `${obj}/latest`);
-  }
-}
-
-/**
- * @description Function validates if the version is valid for semver to process if not it fixes the invalid version to a valid one
- * @param {*} version
- * @returns
- */
-
-async function validateVersion(version) {
-  const segments = version.split('.');
-  const length = segments.length;
-
-  if (length < 3) {
-    while (segments.length < 3) {
-      segments.push('0');
-    }
-    return segments.join('.');
-  } else {
-    return version;
   }
 }
