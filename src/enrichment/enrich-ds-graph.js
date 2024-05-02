c8161e187045e4eac7a946315c3e8b11b260501c
Josef Hardi <josef.hardi@gmail.com>
2024-05-02 15:49:39 -0700

    Fix space size to 2

--- src/enrichment/enrich-ds-graph.js
+++ src/enrichment/enrich-ds-graph.js
@@ -3,72 +3,72 @@ import { resolve } from 'path';
 import { info, more } from '../utils/logging.js';
 import { convert, merge } from '../utils/robot.js';
 import {
-    cleanTemporaryFiles,
-    collectEntities,
-    convertNormalizedDataToOwl,
-    convertNormalizedMetadataToRdf,
-    excludeTerms,
-    extractClassHierarchy,
-    extractOntologySubset,
-    isFileEmpty,
-    logOutput,
-    push
+  cleanTemporaryFiles,
+  collectEntities,
+  convertNormalizedDataToOwl,
+  convertNormalizedMetadataToRdf,
+  excludeTerms,
+  extractClassHierarchy,
+  extractOntologySubset,
+  isFileEmpty,
+  logOutput,
+  push
 } from './utils.js';
 
 export function enrichDatasetGraphMetadata(context) {
-    const { selectedDigitalObject: obj } = context;
-    const normalizedPath = resolve(obj.path, 'normalized/normalized-metadata.yaml');
-    const enrichedPath = resolve(obj.path, 'enriched/enriched-metadata.ttl');
-    convertNormalizedMetadataToRdf(context, normalizedPath, enrichedPath);
+  const { selectedDigitalObject: obj } = context;
+  const normalizedPath = resolve(obj.path, 'normalized/normalized-metadata.yaml');
+  const enrichedPath = resolve(obj.path, 'enriched/enriched-metadata.ttl');
+  convertNormalizedMetadataToRdf(context, normalizedPath, enrichedPath);
 }
 
 export async function enrichDatasetGraphData(context) {
-    try {
-        const { selectedDigitalObject: obj } = context;
-        const normalizedPath = resolve(obj.path, 'normalized/normalized.yaml');
-        const baseInputPath = resolve(obj.path, 'enriched/base-input.ttl');
-        convertNormalizedDataToOwl(context, normalizedPath, baseInputPath);
+  try {
+    const { selectedDigitalObject: obj } = context;
+    const normalizedPath = resolve(obj.path, 'normalized/normalized.yaml');
+    const baseInputPath = resolve(obj.path, 'enriched/base-input.ttl');
+    convertNormalizedDataToOwl(context, normalizedPath, baseInputPath);
 
-        // Extract terms from reference ontologies to enrich the graph data
-        const ontologyExtractionPaths = [];
-        push(ontologyExtractionPaths, baseInputPath); // Set the base input path as the initial
-        push(ontologyExtractionPaths, extractOntologySubset(
-            context, 'uberon', baseInputPath,
-            ["BFO:0000050", "RO:0001025"] // part of, located in
-        ));
+    // Extract terms from reference ontologies to enrich the graph data
+    const ontologyExtractionPaths = [];
+    push(ontologyExtractionPaths, baseInputPath); // Set the base input path as the initial
+    push(ontologyExtractionPaths, extractOntologySubset(
+      context, 'uberon', baseInputPath,
+      ["BFO:0000050", "RO:0001025"] // part of, located in
+    ));
 
-        const fmaEntitiesPath = collectEntities(context, 'fma', baseInputPath);
-        if (!isFileEmpty(fmaEntitiesPath)) {
-            info('Extracting FMA.');
-            const fmaExtractPath = extractClassHierarchy(
-                context,
-                'fma',
-                'http://purl.org/sig/ont/fma/fma62955',
-                fmaEntitiesPath
-            );
-            push(ontologyExtractionPaths, fmaExtractPath);
-        }
-
-        info('Merging files:');
-        for (const inputPath of ontologyExtractionPaths) {
-            more(` -> ${inputPath}`);
-        }
-        const enrichedWithOntologyPath = resolve(obj.path, 'enriched/enriched-with-ontology.owl');
-        merge(ontologyExtractionPaths, enrichedWithOntologyPath);
-
-        const trimmedOutputPath = resolve(obj.path, 'enriched/trimmed-output.ttl');
-        info(`Excluding unwanted terms.`);
-        excludeTerms(context, enrichedWithOntologyPath, trimmedOutputPath);
-
-        const enrichedPath = resolve(obj.path, 'enriched/enriched.ttl');
-        info(`Creating ds-graph: ${enrichedPath}`);
-        convert(trimmedOutputPath, enrichedPath, 'ttl');
-    } catch (e) {
-        error(e);
-    } finally {
-        // Clean up
-        info('Cleaning up temporary files.');
-        cleanTemporaryFiles(context);
-        info('Done.');
+    const fmaEntitiesPath = collectEntities(context, 'fma', baseInputPath);
+    if (!isFileEmpty(fmaEntitiesPath)) {
+      info('Extracting FMA.');
+      const fmaExtractPath = extractClassHierarchy(
+        context,
+        'fma',
+        'http://purl.org/sig/ont/fma/fma62955',
+        fmaEntitiesPath
+      );
+      push(ontologyExtractionPaths, fmaExtractPath);
     }
+
+    info('Merging files:');
+    for (const inputPath of ontologyExtractionPaths) {
+      more(` -> ${inputPath}`);
+    }
+    const enrichedWithOntologyPath = resolve(obj.path, 'enriched/enriched-with-ontology.owl');
+    merge(ontologyExtractionPaths, enrichedWithOntologyPath);
+
+    const trimmedOutputPath = resolve(obj.path, 'enriched/trimmed-output.ttl');
+    info(`Excluding unwanted terms.`);
+    excludeTerms(context, enrichedWithOntologyPath, trimmedOutputPath);
+
+    const enrichedPath = resolve(obj.path, 'enriched/enriched.ttl');
+    info(`Creating ds-graph: ${enrichedPath}`);
+    convert(trimmedOutputPath, enrichedPath, 'ttl');
+  } catch (e) {
+    error(e);
+  } finally {
+    // Clean up
+    info('Cleaning up temporary files.');
+    cleanTemporaryFiles(context);
+    info('Done.');
+  }
 }
--- src/utils/oaktool.js
+++ src/utils/oaktool.js
@@ -1,8 +1,8 @@
 import { throwOnError } from './sh-exec.js';
 
 export function ancestors(scheme = "", path = "", termFile, predicates = [], output) {
-    throwOnError(
-        `runoak -i ${scheme}${path} ancestors \`${`cat ${termFile} | tr -d '\r' | tr '\n' ' ' && echo ''`}\` -p i,${predicates.join()} -o ${output}`,
-        `Getting ancestors failed.`
-    );
+  throwOnError(
+    `runoak -i ${scheme}${path} ancestors \`${`cat ${termFile} | tr -d '\r' | tr '\n' ' ' && echo ''`}\` -p i,${predicates.join()} -o ${output}`,
+    `Getting ancestors failed.`
+  );
 }
