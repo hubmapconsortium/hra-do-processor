ce16bde53b72d3a7cd19bc7401d504461094950e
Josef Hardi <josef.hardi@gmail.com>
2025-02-20 11:03:31 -0800

    Improve execution logging to debug better

--- scripts/setup-schemas.sh
+++ scripts/setup-schemas.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-set -ev
+
+# Turn off command echoing/tracing
+set +x
+set +v
+set -e
 
 # Process the edit version of the LinkML schema
 for schemaFile in schemas/src/{metadata,modules,digital-objects}/*.yaml; do
   type=$(basename ${schemaFile%.yaml})
-  echo $type $schemaFile
+  echo -n "Processing $schemaFile..."
 
   # Generate resolved LinkML schema definitions
   mkdir -p schemas/generated/linkml
-  gen-linkml -f yaml --no-materialize-attributes --materialize-patterns $schemaFile > schemas/generated/linkml/${type}.yaml
+  gen-linkml -f yaml --no-materialize $schemaFile > schemas/generated/linkml/${type}.yaml
+  echo -e "\rProcessing $schemaFile... done"
 done
 
 # Initialize README.md for docs dir
@@ -23,32 +28,46 @@ echo "## Digital Object Types:" >> $README
 # Use the resolved LinkML schema to generate the other data schemas
 for genSchemaFile in schemas/generated/linkml/*.yaml; do
   type=$(basename ${genSchemaFile%.yaml})
+  echo Processing $type
 
+  # Insert to README.md
   echo "- [$type]($type/)" >> $README
 
   # Generate JSON Schema for validation
+  echo -n "-> Generating JSON Schema..."
   mkdir -p schemas/generated/json-schema
   gen-json-schema --title-from title $genSchemaFile > schemas/generated/json-schema/${type}.schema.json
+  echo -e "\r-> Generating JSON Schema... done"
 
   # Generate JSON-LD Context
+  echo -n "-> Generating JSON-LD Context..."
   mkdir -p schemas/generated/json-ld
   gen-jsonld-context $genSchemaFile > schemas/generated/json-ld/${type}.context.jsonld
+  echo -e "\r-> Generating JSON-LD Context... done"
 
   # Generate Markdown documentation
+  echo -n "-> Generating Markdown documentation..."
   mkdir -p schemas/generated/docs
   gen-markdown $genSchemaFile --img --index-file README.md -d schemas/generated/docs/${type}
+  echo -e "\r-> Generating Markdown documentation... done"
 
   # Generate OWL schema definitions
+  echo -n "-> Generating OWL schema..."
   mkdir -p schemas/generated/owl
   gen-owl $genSchemaFile > schemas/generated/owl/${type}.owl.ttl
-
+  echo -e "\r-> Generating OWL schema... done"
+  
   # Generate Mermaid diagrams
+  echo -n "-> Generating ER diagram..."
   mkdir -p schemas/generated/erdiagram
   gen-erdiagram --format mermaid $genSchemaFile > schemas/generated/erdiagram/${type}.mmd
+  echo -e "\r-> Generating ER diagram... done"
 
   # In headless environments, these optional commands will fail
+  echo -n "-> Generating SVG and PNG ER diagrams..."
   mmdc -p puppeteer-config.json -b transparent -i schemas/generated/erdiagram/${type}.mmd -o schemas/generated/erdiagram/${type}.svg
   mmdc -p puppeteer-config.json -b white -i schemas/generated/erdiagram/${type}.mmd -o schemas/generated/erdiagram/${type}.png
+  echo -e "\r-> Generating SVG and PNG ER diagrams... done"
 done
 
 # Generate ER diagrams
